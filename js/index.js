db4acff3b0dfcd5a4d2f607fe39dca273ea4f1f3
vishwanath <vishwanathholge18@gmail.com>
2022-06-02 14:13:36 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+const createTodo = (){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div class="card to-do-card">
+    <div class="card-header">
+        <span class="badge rounded-pill bg-danger"></span>
+
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <a class="btn btn-danger">Delete</a>
+    </div>
+</div> */}
